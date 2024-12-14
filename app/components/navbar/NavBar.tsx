1df6ae5f48206b925ab2719e21f3be1bbc80de03
Alex Mac <louisdebore@gmail.com>
2024-12-14 22:05:02 +0600

    fix concert description styles

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -6,10 +6,11 @@ import Artist from "@/app/components/navbar/artist";
 
 const NavBar = () => {
     return (
-        <nav className={clsx("flex flex-row h-[88px] justify-around items-center z-10 border-b-red-600 border-b-4", bgStyle)}>
+        <nav
+            className={clsx("flex flex-row h-[88px] justify-around items-center z-10 border-b-red-600 border-b-4", bgStyle)}>
             <div className={"flex items-end h-full"}>
-                    <Navigation/>
-                    <LocaleSwitcher/>
+                <Navigation/>
+                <LocaleSwitcher/>
             </div>
             <Artist/>
         </nav>
