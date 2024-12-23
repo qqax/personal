4445bdb1b7170b9d62e8a6711d0029356b1555af
Alex Mac <louisdebore@gmail.com>
2024-12-23 14:50:32 +0600

    metadata fixed

--- app/[locale]/concerts/components/concertMenu.tsx
+++ app/[locale]/concerts/components/concertMenu.tsx
@@ -7,9 +7,9 @@ import Calendar from "react-calendar";
 export const ConcertMenu = () => {
     const [showCalendar, setShowCalendar] = useState(false);
     return (<>
-        <Modal show={!showCalendar}>
-            <Calendar/>
-        </Modal>
+        {/*<Modal show={!showCalendar}>*/}
+        {/*    <Calendar/>*/}
+        {/*</Modal>*/}
         <div
             className={"fixed xl:hidden top-[88px] flex justify-around w-full z-10 bg-green-900 bg-opacity-40 backdrop-blur-sm"}>
             <button type={"button"} onClick={() => setShowCalendar(!showCalendar)} className={"px-2 py-1 bg-white bg-opacity-10 hover:bg-opacity-20"}>Calendar
