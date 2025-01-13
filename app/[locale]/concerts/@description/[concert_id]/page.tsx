72a51779be937464385961e4dbe6f86c34c747c6
Alex Mac <louisdebore@gmail.com>
2025-01-13 15:26:58 +0600

    remove unused comment

--- app/[locale]/concerts/@description/[concert_id]/page.tsx
+++ app/[locale]/concerts/@description/[concert_id]/page.tsx
@@ -11,7 +11,6 @@ export default async function ConcertDescription({params}: { params: { concert_i
     const locale = await getLocale();
     const {concert_id} = await params;
     const concertDescription = await fetchConcertDescription(concert_id, locale);
-    // const concertIDs = await fetchConcertIDs();
 
     if (!concertDescription) {
         return null;
