3a183e5e8fbf435f85a9901eec558cc931658ccf
Alex Mac <louisdebore@gmail.com>
2025-01-18 19:43:53 +0600

    added mdx to client component

--- app/[locale]/concerts/@description/[concert_id]/programme.tsx
+++ app/[locale]/concerts/@description/[concert_id]/programme.tsx
@@ -1,8 +1,27 @@
-import {MDXRemote} from "next-mdx-remote/rsc";
+"use client";
+
+import { useState, useEffect } from "react";
+import { jsx, jsxs, Fragment } from "react/jsx-runtime";
+import { evaluate } from "@mdx-js/mdx";
+
+import type { ReactNode } from "react";
+import type { MDXProps } from "mdx/types";
+import type { EvaluateOptions } from "@mdx-js/mdx";
+
+type ReactMDXContent = (props: MDXProps) => ReactNode;
+type Runtime = Pick<EvaluateOptions, "jsx" | "jsxs" | "Fragment">;
+
+const runtime = { jsx, jsxs, Fragment } as Runtime;
 
 export const Programme = ({programme}: {programme: string}) => {
+    const [MdxContent, setMdxContent] = useState<ReactMDXContent>(() => () => null);
+
+    useEffect(() => {
+        evaluate(programme, runtime).then(r => setMdxContent(() => r.default));
+    }, [programme]);
+
   return <>
       <div className={"text-center text-2xl text-beige"}>Programme</div>
-      {/*<MDXRemote source={programme}/>*/}
+      <MdxContent/>
   </>
 }
