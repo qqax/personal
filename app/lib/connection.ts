f772b0edfea916dc5137993abbc186a37dfcc2f4
Alexander Kudryavtsev <alexanderkudryavtsev87@gmail.com>
2025-04-15 00:52:09 +0600

    add neon support

--- app/lib/connection.ts
+++ app/lib/connection.ts
@@ -1,16 +1,33 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/neon-http";
+// import { drizzle } from "drizzle-orm/node-postgres";
+
 import * as common from "@/app/lib/schema/common";
 import * as concert_records from "@/app/lib/schema/concert-records";
 import * as auth from "@/app/lib/schema/auth";
 
-export const db = drizzle({
-    connection: {
-        connectionString: process.env.POSTGRES_URL,
-    },
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
 
+config({ path: ".env" });
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({
+    client: sql,
     schema: {
         ...common,
         ...concert_records,
         ...auth,
     },
 });
+
+// export const db = drizzle({
+//     connection: {
+//         connectionString: process.env.POSTGRES_URL,
+//     },
+//
+//     schema: {
+//         ...common,
+//         ...concert_records,
+//         ...auth,
+//     },
+// });
